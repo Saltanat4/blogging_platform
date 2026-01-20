a6116386899c0a0ed0b33d4025e35d55d04efbc0
Saltanat4 <242242@astanait.edu.kz>
2026-01-21 00:33:55 +0500

    2

--- public/script.js
+++ public/script.js
@@ -3,64 +3,98 @@ async function createBlog() {
   const body = document.querySelector("textarea[name='body']").value;
   const author = document.querySelector("input[name='author']").value;
 
-  await fetch("/blogs", {
+  const response = await fetch("/blogs", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ title, body, author })
   });
 
-  viewAll();
+  const blog = await response.json();
+
+  document.getElementById("post").innerHTML = `
+    <div>
+      <h4>Created post</h4>
+      <p>Title: ${blog.title}</p>
+      <p>Body: ${blog.body}</p>
+      <p>Author: ${blog.author}</p>
+      <p>ID: ${blog._id}</p>
+    </div>
+  `;
 }
 
-async function viewAll(){
-    const response=await fetch("/blogs");
-    const blogs=await response.json();
+async function viewAll() {
+  const response = await fetch("/blogs");
+  const blogs = await response.json();
 
-    const container=document.getElementById("post");
-    container.innerHTML="";
+  const container = document.getElementById("post");
+  container.innerHTML = "<h4>All posts</h4>";
 
-    blogs.forEach(blogs=>{
-        container.innerHTML+=`<div>
-        <h4>Title: ${blogs.title}</h4>
-        <p>Body: ${blogs.body}</p>
-        <p>Author: ${blogs.author}</p>
-        </div>`;
-    });
+  blogs.forEach(blog => {
+    container.innerHTML += `
+      <div>
+        <p><b>${blog.title}</b></p>
+        <p>${blog.body}</p>
+        <p>${blog.author}</p>
+        <p>ID: ${blog._id}</p>
+      </div>
+    `;
+  });
 }
 
-async function viewByID(){
-    const id=document.getElementById("viewid").value;
+async function viewByID() {
+  const id = document.getElementById("viewid").value;
 
-    const response=await fetch("/blogs/${id}")
-    const blog=await response.json();
+  const response = await fetch(`/blogs/${id}`);
+  const blog = await response.json();
 
-    const container=document.getElementById("post");
-    container.innerHTML=`<h3>Title: ${blog.title}</h3>
-    <p>Body: ${blog.body}</p>
-    <p>Author: ${blog.author}</p>`;
+  document.getElementById("post").innerHTML = `
+    <div>
+      <h4>Post by ID</h4>
+      <p>Title: ${blog.title}</p>
+      <p>Body: ${blog.body}</p>
+      <p>Author: ${blog.author}</p>
+      <p>ID: ${blog._id}</p>
+    </div>
+  `;
 }
 
-async function update(){
-    const id=document.getElementById("updateid");
+async function update() {
+  const id = document.getElementById("updateid").value;
+  const title = document.querySelector("input[placeholder='New title']").value;
+  const body = document.querySelector("textarea[placeholder='New content']").value;
 
-    const title=document.querySelector("input[placeholder='New title']").value;
-    const body=document.querySelector("input[placeholder='New content']").value;
+  const response = await fetch(`/blogs/${id}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ title, body })
+  });
 
-    await fetch("/blogs/${id}", {
-        method:"PUT",
-        headers:{"Content-type":"application/json"},
-        body:JSON.stringify({title, body})
-    });
+  const blog = await response.json();
 
-    viewAll();
+  document.getElementById("post").innerHTML = `
+    <div>
+      <h4>Updated post</h4>
+      <p>Title: ${blog.title}</p>
+      <p>Body: ${blog.body}</p>
+      <p>Author: ${blog.author}</p>
+      <p>ID: ${blog._id}</p>
+    </div>
+  `;
 }
 
-async function deleteBlog(){
-    const id=document.getElementById("deleteid").value;
+async function deleteBlog() {
+  const id = document.getElementById("deleteid").value;
 
-    await fetch("blogs/${id}",{
-        method:"DELETE"
-    });
-    viewAll();
+  const response = await fetch(`/blogs/${id}`, {
+    method: "DELETE"
+  });
 
+  const result = await response.json();
+
+  document.getElementById("post").innerHTML = `
+    <div>
+      <h4>${result.message}</h4>
+      <p>ID:${id}</p>
+    </div>
+  `;
 }
